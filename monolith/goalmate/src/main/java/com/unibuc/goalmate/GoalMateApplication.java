b1c0a1420b53696d83937137adf8283d796298b9
andreeav19 <andra_368@yahoo.com>
2025-05-06 11:09:36 +0300

    initial commit

---
+++ monolith/goalmate/src/main/java/com/unibuc/goalmate/GoalMateApplication.java
@@ -0,0 +1,13 @@
+package com.unibuc.goalmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoalMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoalMateApplication.class, args);
+	}
+
+}
