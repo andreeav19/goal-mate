cbc0847b16cc404336c4b3375ff50cd7b3a07ee0
andreeav19 <andra_368@yahoo.com>
2025-05-07 15:11:13 +0300

    initialised db with roles and admin

---
+++ monolith/goalmate/src/main/java/com/unibuc/goalmate/util/DataInitializer.java
@@ -0,0 +1,63 @@
+package com.unibuc.goalmate.util;
+
+import com.unibuc.goalmate.model.GoalMateUser;
+import com.unibuc.goalmate.model.Role;
+import com.unibuc.goalmate.model.RoleName;
+import com.unibuc.goalmate.repository.GoalMateUserRepository;
+import com.unibuc.goalmate.repository.RoleRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+    private final RoleRepository roleRepository;
+    private final GoalMateUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Value("${admin.username}")
+    private String adminUsername;
+
+    @Value("${admin.password}")
+    private String adminPassword;
+
+    @Value("${admin.email}")
+    private String adminEmail;
+
+    @Override
+    public void run(String... args) throws Exception {
+        for (RoleName roleName : RoleName.values()) {
+            if (!roleRepository.existsByRoleName(roleName)) {
+                Role role = new Role();
+                role.setRoleName(roleName);
+                roleRepository.save(role);
+            }
+        }
+
+        if (userRepository.count() == 0) {
+            GoalMateUser adminUser = new GoalMateUser();
+
+            Role adminRole = roleRepository.findByRoleName(RoleName.ADMIN)
+                    .orElseThrow(() -> new EntityNotFoundException("Admin role not found."));
+            Role userRole = roleRepository.findByRoleName(RoleName.USER)
+                    .orElseThrow(() -> new EntityNotFoundException("User role not found."));
+
+            adminUser.setRoles(new HashSet<>(Arrays.asList(adminRole, userRole)));
+            adminUser.setUsername(adminUsername);
+            adminUser.setEmail(adminEmail);
+            adminUser.setPassword(passwordEncoder.encode(adminPassword));
+
+            adminRole.getUsers().add(adminUser);
+            userRole.getUsers().add(adminUser);
+
+            userRepository.save(adminUser);
+        }
+    }
+}
