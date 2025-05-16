5192b1bf3c3b9f8a492dddbb513ed0755d9a6b55
andreeav19 <andra_368@yahoo.com>
2025-05-16 17:22:32 +0300

    added tests for user controller

---
+++ monolith/goalmate/src/test/java/com/unibuc/goalmate/controller/UserControllerTest.java
@@ -0,0 +1,121 @@
+package com.unibuc.goalmate.controller;
+
+import com.unibuc.goalmate.dto.UserResponseDto;
+import com.unibuc.goalmate.dto.UserRoleRequestDto;
+import com.unibuc.goalmate.service.AuthService;
+import com.unibuc.goalmate.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = true)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AuthService authService;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Test
+    @WithMockUser(username = "admin@example.com", roles = {"ADMIN", "USER"})
+    void getAllUsers_AsAdmin_ShouldReturnUsersPage() throws Exception {
+        List<UserResponseDto> users = List.of(
+                new UserResponseDto(1L,
+                        "admin",
+                        "admin@example.com",
+                        List.of("ADMIN", "USER"),
+                        List.of(),
+                        false)
+        );
+
+        when(userService.getAllUsers("admin@example.com")).thenReturn(users);
+        when(authService.isCurrentUserAdmin()).thenReturn(true);
+
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin/users"))
+                .andExpect(model().attribute("isAdmin", true))
+                .andExpect(model().attribute("users", users))
+                .andExpect(model().attributeExists("userRoleRequest"));
+    }
+
+    @Test
+    void getAllUsers_NotAuthenticated_ShouldNotReturnUsersPage() throws Exception {
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "admin@example.com", roles = {"ADMIN", "USER"})
+    void addRole_ValidInput_ShouldCallServiceAndRedirect() throws Exception {
+        UserRoleRequestDto dto = new UserRoleRequestDto(2L, "ADMIN");
+
+        mockMvc.perform(post("/admin/add-role")
+                        .flashAttr("userRoleRequest", dto)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/admin"));
+
+        verify(userService, times(1)).addUserRole(any(UserRoleRequestDto.class));
+    }
+
+    @Test
+    @WithMockUser(username = "admin@example.com", roles = {"ADMIN", "USER"})
+    void addRole_InvalidInput_ShouldNotCallServiceAndRedirect() throws Exception {
+        UserRoleRequestDto invalidDto = new UserRoleRequestDto(null, "");
+
+        mockMvc.perform(post("/admin/add-role")
+                        .flashAttr("userRoleRequest", invalidDto)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/admin"));
+
+        verify(userService, never()).addUserRole(any());
+    }
+
+    @Test
+    @WithMockUser(username = "admin@example.com", roles = {"ADMIN", "USER"})
+    void deleteRole_ValidInput_ShouldCallServiceAndRedirect() throws Exception {
+        UserRoleRequestDto dto = new UserRoleRequestDto(2L, "ADMIN");
+
+        mockMvc.perform(post("/admin/delete-role")
+                        .flashAttr("userRoleRequest", dto)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/admin"));
+
+        verify(userService, times(1)).deleteUserRole(any(UserRoleRequestDto.class));
+    }
+
+    @Test
+    @WithMockUser(username = "admin@example.com", roles = {"ADMIN", "USER"})
+    void deleteRole_InvalidInput_ShouldNotCallServiceAndRedirect() throws Exception {
+        UserRoleRequestDto invalidDto = new UserRoleRequestDto(null, "");
+
+        mockMvc.perform(post("/admin/delete-role")
+                        .flashAttr("userRoleRequest", invalidDto)
+                        .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/admin"));
+
+        verify(userService, never()).deleteUserRole(any());
+    }
+
+}
