cf7e9d1cbf65e6543735d918999610daad1ccbf1
andreeav19 <andra_368@yahoo.com>
2025-06-17 15:42:10 +0300

    add empty gateway service

--- microservices/auth-service/src/main/java/com/example/auth_service/security/SecurityConfig.java
+++ microservices/auth-service/src/main/java/com/example/auth_service/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
---
+++ microservices/gateway-service/src/main/java/com/example/gateway_service/security/GatewaySecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.gateway_service.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain() {
+        return ServerHttpSecurity.http()
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().permitAll()
+                )
+                .build();
+    }
+}
+
